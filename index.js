5987fb4646622a271e944dc07c9a6f2c82ab887f
talexcrowell <talexcrowell@gmail.com>
2018-12-21 08:53:53 -0800

    added routers and sample data

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const dogRouter = require('./routers/dogs');
+const catRouter = require('./routers/cats');
+
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
@@ -22,29 +25,9 @@ app.use(
   })
 );
 
-app.get('/api/cat', (req,res,next)=> {
-  return res.json({
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  });
-});
+app.use('/api/dog', dogRouter);
+app.use('/api/cat', catRouter);
 
-app.get('/api/dog', (req, res, next) => {
-  return res.json({  
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
-  });
-});
 
 function runServer(port = PORT) {
   const server = app
