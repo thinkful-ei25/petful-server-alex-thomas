ff226f93b34111f1b4b144c71f8c27c71ac05070
talexcrowell <talexcrowell@gmail.com>
2018-12-21 08:10:26 -0800

    added dog and cat endpoints

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ app.use(
   })
 );
 
+app.get('/api/cat', (req,res,next)=> {
+  return res.json({
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  });
+});
+
+app.get('/api/dog', (req, res, next) => {
+  return res.json({  
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  });
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
